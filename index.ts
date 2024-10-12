588dcf131a9958402aecc6185c6190d3a72c4220
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-10-12 16:48:58 +0700

    tạo route cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("oke")
+import express,{Express,Request, Response} from "express"
+import { ppid } from "process";
+
+const app: Express = express();
+const port: Number = 3000;
+app.get("/topics",(req: Request, res: Response)=>{
+    res.send("chủ đề bài hát");
+})
+
+app.listen(port,()=>{
+    console.log(`app listening on port ${port}`)
+})
