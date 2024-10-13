c9ee7238470ed45ab663698f3204beabff21c207
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-10-13 16:47:40 +0700

    tạo router

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express,{Express,Request, Response} from "express"
+import express,{Express} from "express"
 import dotenv from "dotenv"
 import * as database from "./config/database";
-import Topic from "./model/topic.model";
+import clientRoutes from "./routes/client/index.route";
 dotenv.config();
 database.connect();
 
@@ -11,11 +11,7 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views","./views")
 app.set("view engine","pug")
 
-app.get("/topics", async (req: Request, res: Response)=>{
-    const topics = await Topic.find({ deleted: false})
-    console.log(topics)
-    res.render("./client/pages/topics/index")
-})
+clientRoutes(app);
 
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`)
---
+++ routes/client/topic.route.ts
@@ -0,0 +1,14 @@
+import { Request,Response,Router } from "express";
+import Topic from "../../model/topic.model";
+const router: Router = Router();
+
+router.get("/",async (req: Request, res: Response)=>{
+    const topics = await Topic.find({
+        deleted: false,
+    })
+    console.log(topics)
+    
+    res.render("client/pages/topics/index")
+})
+
+export const TopicRoutes: Router = router;
