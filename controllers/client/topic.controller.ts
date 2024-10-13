e9edca7c3c21307338ef337b669c7a2c9be3035c
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-10-13 19:41:14 +0700

    tạo controller

---
+++ controllers/client/topic.controller.ts
@@ -0,0 +1,12 @@
+import {Response, Request} from "express"
+import Topic from "../../model/topic.model"
+
+// [GET] topics
+export const  topics = async (req: Request, res: Response)=>{
+    const topics = await Topic.find({
+        deleted: false,  
+    })
+    console.log(topics)
+    
+    res.render("client/pages/topics/index")
+}
--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,14 +1,7 @@
 import { Request,Response,Router } from "express";
-import Topic from "../../model/topic.model";
+import * as controller  from "../../controllers/client/topic.controller";
 const router: Router = Router();
 
-router.get("/",async (req: Request, res: Response)=>{
-    const topics = await Topic.find({
-        deleted: false,
-    })
-    console.log(topics)
-    
-    res.render("client/pages/topics/index")
-})
+router.get("/",controller.topics)
 
 export const TopicRoutes: Router = router;
