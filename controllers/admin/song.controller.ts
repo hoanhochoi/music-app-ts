85ce720f4f6aa57b2fd8bd5470bc2c7c440c367f
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-11-06 21:13:06 +0700

    làm giao diện thêm mới p1

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,5 +1,7 @@
 import {Response,Request} from "express";
 import Song from "../../model/song.model";
+import Topic from "../../model/topic.model";
+import Singer from "../../model/singer.model";
 
 //[GET] admin/songs
 export const index = async (req:Request,res:Response)=>{
@@ -9,5 +11,24 @@ export const index = async (req:Request,res:Response)=>{
     res.render("./admin/pages/songs/index.pug",{
         pageTitle: "Quản lý bài hát",
         songs: songs
+    })
+}
+
+// [GET] admin/songs/create
+export const create = async (req: Request,res:Response)=>{
+    const topics = await Topic.find({
+        deleted: false,
+        status: "active"
+    }).select("title");
+
+    const singers = await Singer.find({
+        deleted: false,
+        status: "active"
+    }).select("fullName");
+
+    res.render("./admin/pages/songs/create",{
+        pageTitle: "Thêm mới bài hát",
+        topics: topics,
+        singers: singers,
     })
 }
