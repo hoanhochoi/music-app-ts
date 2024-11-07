d76437cf46e90e588894aa1191765b1370f816e3
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-11-07 10:19:44 +0700

    phân trang quản lý chủ để (admin)

--- controllers/admin/topic.controller.ts
+++ controllers/admin/topic.controller.ts
@@ -1,14 +1,27 @@
 import {Response,Request} from "express";
 import Topic from "../../model/topic.model";
+import PaginationHelper from "../../helpers/pagination";
 
 // [GET] /admin/topics
 export const index = async (req:Request,res: Response)=>{
+
+    const countTopic = await Topic.countDocuments({
+        deleted: false,
+    })
+    let initPagination = {
+        currentPage : 1,
+        limitItems: 3,
+    }
+    const objectPagination = PaginationHelper(initPagination,req.query,countTopic);
+
     const topics = await Topic.find({
         deleted: false,
-    });
-    console.log(topics);
+    }).limit(objectPagination.limitItems).skip(objectPagination.skip);
+    console.log(objectPagination)
+    // console.log(topics);
     res.render("admin/pages/topics/index",{
         pageTitle: "Quản lý chủ đề",
         topics: topics,
+        pagination: objectPagination
     })
 }
